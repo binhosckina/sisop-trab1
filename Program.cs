d9d82d574c8a7de24381504d80cb4f2072ddf693
Gabriel Correa <gabriel.correa@sap.com>
2021-03-11 18:23:34 -0300

    setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sisop_trab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
